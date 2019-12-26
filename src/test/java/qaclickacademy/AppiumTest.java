158635973ac7b260cce2e411f4e56ea95180d2fc
Sujith Kumar <suji118@gmail.com>
2019-12-26 17:05:26 +0530

    updated appium test cases

--- src/test/java/qaclickacademy/AppiumTest.java
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -7,5 +7,13 @@ public class AppiumTest {
 	@Test
 	public void mobile() {
 		System.out.println("Mobile");
+		
+		@Test
+		public void mobile1() {
+			System.out.println("Mobile");
+			
+			@Test
+			public void mobile2() {
+				System.out.println("Mobile");
 	}
 }
